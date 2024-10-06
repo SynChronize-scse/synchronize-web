102dae0a3005f78bcc7160b9a1d05b0f6bb4cc9c
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2024-10-06 15:46:03 +0530

    chore: adds landing layout file

---
+++ src/components/Home/LandingSection.tsx
@@ -0,0 +1,7 @@
+function LandingSection() {
+  return (
+    <h1 className="text-5xl font-bold text-primary-400 font-[AdieuRegular]">Synchronize'25</h1>
+  )
+}
+
+export default LandingSection
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import Footer from "../components/Footer";
+import LandingSection from "../components/Home/LandingSection";
+
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-dark-400 text-white">
-      <h1 className="text-5xl font-bold text-primary-400 font-[AdieuRegular]">Synchronize'25</h1>
+      <main>
+        <LandingSection />
+      </main>
+
+      <Footer />
     </div>
   );
 }
