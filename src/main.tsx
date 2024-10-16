e7a694cdfa9494497dd7016108339aef2b5d2fad
Bismay <bismaysarangi@gmail.com>
2024-10-16 22:12:56 +0530

    added component: footer

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/main.css'
+import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
 )
