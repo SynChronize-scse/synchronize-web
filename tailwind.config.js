cabe3e646cbc33ac3dd0bcea22b3af976ac27577
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2024-10-25 19:26:34 +0530

    fix: bad commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,8 @@ export default {
         'custom-cursor': 'url("/images/cursor.png"), auto',
       },
       animation: {
-        fadeInSlide: 'fadeInSlide 0.5s ease-out forwards', // New animation
+        fadeInSlide: 'fadeInSlide 0.5s ease-out forwards',
+        bounceMore: 'bounceMore 1.5s infinite'
       },
       keyframes: {
         fadeInSlide: {  // Keyframes for fading in and sliding up
@@ -34,6 +35,16 @@ export default {
           '100%': {
             opacity: '1',
             transform: 'translateY(0)',  // End in place
+          }
+        },
+        bounceMore:  {
+          '0%, 100%':{
+            transform: 'translateY(-25%)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(100%)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
           }
         }
       }
