e58a548f162354e519aa5a7291cdb6ea0ec32d7b
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2024-10-26 22:45:53 +0530

    chore: disables lenis for /memories route

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,16 @@ import Footer from "$lib/components/Footer.tsx";
 import { ReactLenis } from "lenis/react";
 import "lenis/dist/lenis.css";
 
+const ignoreRoutes = ["/memories"];
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ReactLenis root>
+    <ReactLenis
+      root
+      options={{
+        smoothWheel: !ignoreRoutes.includes(window.location.pathname),
+      }}
+    >
       <App />
       <Footer />
     </ReactLenis>
