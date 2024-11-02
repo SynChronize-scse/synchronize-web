bb1f72aa1daee5173bb1f343c4e5efd338329452
Anupam0-0 <anupamchoudhary04@gmail.com>
2024-11-02 12:16:52 +0530

    minor improvements

--- src/pages/EventsPage/Components/FunNcultural.jsx
+++ src/pages/EventsPage/Components/FunNcultural.jsx
@@ -1,31 +1,23 @@
-import React from 'react'
-import CardSection from './CardSection'
-import { Link } from 'react-router-dom'
+import React from "react";
+import CardSection from "./CardSection";
+import { Link } from "react-router-dom";
+import EventsMenu from "./EventsMenu";
 
 const FunNcultural = () => {
-    return (
-        <div className='flex flex-col items-center pb-10 bg-slate-900'>
+  return (
+    <div className="flex flex-col items-center pb-10 bg-slate-950">
+      <EventsMenu />
 
-            <div className='h-fit p-4 mt-2 w-[92vw] rounded-lg border-2 border-orange-200 flex flex-row justify-around '>
-                <Link to='/events'><div className='text-xl text-orange-200'>Centerstage Events </div> {/* Active */}</Link>
-                <Link to='events/funncultural'><div className='text-xl text-orange-200'>Fun and Cultural</div></Link>
-                <div className='text-xl text-orange-200'>Workshop and Guest Talks</div>
-            </div>
+      <div className="w-[90vw] -ml-8 flex flex-col items-start justify-evenly gap-10 text-orange-200">
+        <CardSection title="🕹️ VIDEO GAMES 🎮" />
+        <CardSection title="🧩 MAZE HUNT" />
+        <CardSection title="📎 ORIGAMI" />
+        <CardSection title="📸 PHOTOBOOTH" />
+        <CardSection title="🎙️ MUSIC" />
+        <CardSection title="🕺 DANCE COMPETITION" />
+      </div>
+    </div>
+  );
+};
 
-            <div className='w-[90vw] -ml-8 flex flex-col items-start justify-evenly gap-10 text-orange-200'>
-                <CardSection title="🕹️ VIDEO GAMES 🎮" />
-                <CardSection title="🧩 MAZE HUNT" />
-                <CardSection title="📎 ORIGAMI" />
-                <CardSection title="📸 PHOTOBOOTH" />
-                <CardSection title="🎙️ MUSIC" />
-                <CardSection title="🕺 DANCE COMPETITION" />
-            </div>
-
-
-        </div>
-
-
-    )
-}
-
-export default FunNcultural
+export default FunNcultural;
