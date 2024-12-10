6e0e6e7ff2e9f1ec1ace6bd0ee775eada6a4ac43
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2024-12-10 18:12:45 +0530

    -

---
+++ src/lib/data/events.ts
@@ -0,0 +1,223 @@
+export const FLAGSHIP = [
+  {
+    title: "24 hours hackathon",
+    description:
+      "A thrilling 24-hour hackathon designed for developers of all levels. \nTest your coding skills and creativity as you collaborate with like-minded individuals. \nCompete for exciting prizes while building innovative projects under a tight deadline.",
+    img: "https://source.unsplash.com/featured/?hackathon",
+    // link: "/events/hackathon"  TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "11:00 AM",
+  },
+];
+
+export const ROBOTICS = [
+  {
+    title: "Zenos Terminus",
+    description:
+      "Join the ultimate robotics race in 'Zenos Terminus,' where speed, precision, and control are key. \nDesign and program your robot to race through a challenging course full of obstacles. \nThe fastest robot wins, but navigating the course perfectly is just as important.",
+    img: "https://source.unsplash.com/featured/?robotics",
+    // link: "/events/robotics"  TO BE REPLACED W/ MODAL
+    date: "09-01-2024",
+    time: "12:00 NOON",
+  },
+  {
+    title: "Megatron",
+    description:
+      "'Megatron' is a fierce robot combat competition where participants build robots to battle in a controlled arena. \nStrategize and optimize your robot for combat, outsmarting your opponents for the title. \nGet ready for an action-packed, high-stakes showdown!",
+    img: "https://source.unsplash.com/featured/?robotics",
+    // link: "/events/robotics"  TO BE REPLACED W/ MODAL
+    date: "09-01-2024",
+    time: "03:00 PM",
+  },
+  {
+    title: "Path Xplorer",
+    description:
+      "'Path Xplorer' challenges you to build a robot that can find the shortest path through a maze. \nNavigate obstacles and optimize your robot’s path to reach the finish line first. \nPut your problem-solving and programming skills to the test in this exciting event.",
+    img: "https://source.unsplash.com/featured/?robotics",
+    // link: "/events/robotics"  TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "10:45 AM",
+  },
+];
+
+export const CODING = [
+  {
+    title: "Debugging Challenge",
+    description:
+      "In the 'Debugging Challenge,' participants fix broken code under time pressure. \nIdentify bugs quickly and solve them for the chance to win exciting prizes. \nYour debugging skills will be tested as you race against the clock.",
+    img: "https://source.unsplash.com/featured/?coding",
+    // link: "/events/coding"  TO BE REPLACED W/ MODAL
+    date: "09-01-2024",
+    time: "11:45 AM",
+  },
+  {
+    title: "Clash of Codes",
+    description:
+      "'Clash of Codes' is a coding competition where you solve timed challenges against others. \nShow off your coding skills as you compete to solve algorithmic problems. \nThe fastest and most efficient solution wins the competition.",
+    img: "https://source.unsplash.com/featured/?coding",
+    // link: "/events/coding"  TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "02:00 PM",
+  },
+  {
+    title: "Algorhym Master",
+    description:
+      "'Algorhym Master' is an algorithm competition that tests your problem-solving and coding abilities. \nSolve complex algorithmic challenges within a fixed time. \nThink critically and use your coding skills to claim victory.",
+    img: "https://source.unsplash.com/featured/?coding",
+    // link: "/events/coding"  TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "03:00 PM",
+  },
+  {
+    title: "Promptopia",
+    description:
+      "'Promptopia' challenges you to write the best code under strict time constraints. \nSolve unique coding problems while ensuring your code is efficient and effective. \nRace against the clock to prove your coding prowess!",
+    img: "https://source.unsplash.com/featured/?coding",
+    // link: "/events/coding"  TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "01:00 AM",
+  },
+];
+
+export const BUSINESS = [
+  {
+    title: "Stock Simulation",
+    description:
+      "'Stock Simulation' is a virtual stock trading competition. \nBuy and sell stocks based on real-time data to grow your portfolio. \nManage risks, predict market trends, and maximize your profits to win.",
+    img: "https://source.unsplash.com/featured/?business",
+    // link: "/events/business"  TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "10:00 AM",
+  },
+  {
+    title: "Tech Tank",
+    description:
+      "In 'Tech Tank,' entrepreneurs pitch their business ideas to investors. \nPresent your product, receive feedback, and compete for investment or mentorship. \nShowcase your innovation and take your startup to the next level!",
+    img: "https://source.unsplash.com/featured/?business",
+    // link: "/events/business"  TO BE REPLACED W/ MODAL
+    date: "11-01-2024",
+    time: "11:00 AM",
+  },
+];
+
+export const FUN = [
+  // FUN & CULTURAL
+  {
+    title: "Treasure Hunt",
+    description:
+      "Gear up for an adventurous quest to solve clues and find the hidden treasure. Navigate through challenges with your team and emerge victorious. Exciting prizes await the winners. Let the hunt begin!",
+    img: "https://source.unsplash.com/featured/?fun",
+    date: "09-01-2024",
+    time: "04:30 PM",
+  },
+  {
+    title: "Tech Skibble",
+    description:
+      "Bring out the artist in you by sketching creative, tech-inspired masterpieces. Impress the judges with your artistic flair. Top entries win amazing rewards. Let your imagination flow!",
+    img: "https://source.unsplash.com/featured/?fun",
+    date: "11-01-2024",
+    time: "02:00 PM",
+  },
+  {
+    title: "Tech Bingo",
+    description:
+      "Experience the excitement of Bingo with a tech twist. Match the numbers and shout 'Bingo!' to win cool prizes. Gather your friends and make it a memorable game day.",
+    img: "https://source.unsplash.com/featured/?fun",
+    date: "11-01-2024",
+    time: "2:00 PM",
+  },
+];
+
+export const GAMING = [
+  {
+    title: "Valorant",
+    description:
+      "Showcase your gaming skills in this intense first-person shooter. Teamwork and strategy are key to securing victory. Compete against the best and take home amazing prizes.",
+    img: "https://source.unsplash.com/featured/?valorant",
+    date: "10-01-2024",
+    time: "Pls check the event page",
+  },
+  {
+    title: "FIFA",
+    description:
+      "Step onto the virtual football field and prove your prowess. Compete head-to-head to score goals and win. Amazing prizes await the top players.",
+    img: "https://source.unsplash.com/featured/?fifa",
+    date: "11-01-2024",
+    time: "Pls check the event page",
+  },
+  {
+    title: "BGMI",
+    description:
+      "Enter the battleground for the ultimate survival game. Show your strategic brilliance and shooting skills. Winners will grab thrilling prizes and bragging rights.",
+    img: "https://source.unsplash.com/featured/?bgmi",
+    date: "11-01-2024",
+    time: "Pls check the event page",
+  },
+];
+
+export const EVERYDAY = [
+  // Everyday events
+  {
+    title: "Pose Pavilion",
+    description:
+      "Snap stunning pictures at our lively photobooth. Add filters, props, and poses to create cherished memories. Take your printed photos home as souvenirs.",
+    img: "https://source.unsplash.com/featured/?photobooth",
+    date: "EVERYDAY EVENT",
+    time: "ALL DAY",
+  },
+  {
+    title: "Game Stalls",
+    description:
+      "Try your luck and skills at our fun-filled game stalls. Engage in interactive games, win exciting prizes, and make unforgettable memories with your friends.",
+    img: "https://source.unsplash.com/featured/?game",
+    date: "EVERYDAY EVENT",
+    time: "ALL DAY",
+  },
+  {
+    title: "Bite Bazaar",
+    description:
+      "Enjoy a delightful spread of food at our stalls. From local delicacies to global cuisines, there's something for everyone. Come hungry, leave happy!",
+    img: "https://source.unsplash.com/featured/?food",
+    date: "EVERYDAY EVENT",
+    time: "ALL DAY",
+  },
+  {
+    title: "Startup Parks",
+    description:
+      "Explore cutting-edge innovations at our startup parks. Interact with budding entrepreneurs, discover their ideas, and get inspired by their journeys.",
+    img: "https://source.unsplash.com/featured/?startup",
+    date: "EVERYDAY EVENT",
+    time: "ALL DAY",
+  },
+];
+
+export const GUESTLECTURES = [
+  //Workshops & Guest Lectures
+  {
+    title: "GUest Talk (IEEE)",
+    Guest: "Dr. Bruce Banner",
+    description: "Guest talk by IEEE.",
+    img: "https://source.unsplash.com/featured/?guest",
+    // link: "/events/guesttalk"     TO BE REPLACED W/ MODAL
+    date: "09-01-2024",
+    time: "10:50 AM",
+  },
+  {
+    title: "Workshop (ACM)",
+    Guest: "Dr. Tony Stark",
+    description: "Workshop by ACM.",
+    img: "https://source.unsplash.com/featured/?workshop",
+    // link: "/events/workshop"     TO BE REPLACED W/ MODAL
+    date: "10-01-2024",
+    time: "10:00 AM",
+  },
+  {
+    title: "Youth Icon Talk ",
+    Guest: "Mr. Steve Rogers",
+    description: "Talk by Youth Icon.",
+    img: "https://source.unsplash.com/featured/?youth",
+    // link: "/events/youthtalk"     TO BE REPLACED W/ MODAL
+    date: "11-01-2024",
+    time: "10:00 AM",
+  },
+];
